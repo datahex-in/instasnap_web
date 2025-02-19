075c651169631fd29d24505197a4e16a7c74ceac
Mohammed Faris M A <mfaris2k18@gmail.com>
2025-02-19 13:39:42 +0530

    Update 2.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2459300478"
+    serviceWorkerVersion: "4225495636"
   }
 });
